f8824d37d42771cf18f719e5f6657e4dd9060087
Arcandrus <eric.harper87@gmail.com>
2025-01-30 15:12:11 +0000

    startGame() Now generates 4 pairs of cards when called.

--- game.js
+++ game.js
@@ -6,19 +6,28 @@ function startGame() {
   let cards = [];
   let i = 0;
 
-  // Add 4 cards to the Array
+  // Add 4 pairs to the Array
   while (i < 4) {
+    // Choose card color
     let chooseColor = Math.floor(Math.random() * colors.length)
+    // Assign values to card
     let card = {
       id: i,
       color: colors[chooseColor],
-      flipped: false
     };
+    // Add two of that card to the array
     cards.push(card);
+    cards.push(card);
+    // Remove the chosen color from the array to prevent duplication
     colors.splice(chooseColor, 1);
+    // Increment counter
     i++;
   }
-
+  // Set all cards to facce down
+  for (i in cards) {
+    cards[i].flipped = false;
+  }
+  // Console log for testing
   console.log(`Generated ${cards.length} cards`)
   for (i in cards) {
     console.log(`ID ${cards[i].id}`)

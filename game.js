c3265b50524b68c9864025849fb25a25c1f6164a
Arcandrus <eric.harper87@gmail.com>
2025-01-31 14:25:57 +0000

    onClick function to 'flip' facedown cards now implemented, for testing purposes in bolds the text of the chosen cards. Currently calling a function to checkMatch(), not yet implemented

--- game.js
+++ game.js
@@ -1,13 +1,30 @@
 let cards = [];
+let matches = 0;
+let cardFirst = null;
+let cardSecond = null;
 
 function startGame() {
+  // Reset control variables
+  cards = [];
+  matches = 0;
+  cardFirst = null;
+  cardSecond = null;
+  // Reset the UI
+  matchDisplay.innerText = `Matches: ${matches}`;
+  cardContainer.innerHTML = ``;
+  //Gemerate cards
   generateCards()
+  // Shuffle the cards
+  shuffleCards(cards);
+  // Render the cards to the browser
+  renderCards(cards);
 }
 
+// Generate X number of card pairs (4 for testing)
 function generateCards() {
   let colors = ["White", "Blue", "Black", "Red", "Green"]
   // Reset the card Array
-  let cards = [];
+  cards = [];
   let i = 0;
   // Add 4 pairs to the Array
   while (i < 4) {
@@ -17,6 +34,7 @@ function generateCards() {
     let card = {
       id: i,
       color: colors[chooseColor],
+      flipped: false
     };
     // Add two of that card to the array
     cards.push(card, card);
@@ -25,29 +43,60 @@ function generateCards() {
     // Increment counter
     i++;
   }
-  // Set all cards to face down
-  for (i in cards) {
-    cards[i].flipped = false;
-  }
-  shuffleCards(cards);
-  renderCards(cards);
 }
 
 function shuffleCards(cards) {
   for (let i = cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [cards[i], cards[j]] = [cards[j], cards[i]]; // Swap elements
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]]; // Swap elements
   }
 }
 
+// Render the cards to the browser
 function renderCards(cards) {
+  // Reset the card display
   cardContainer.innerHTML = ``;
+  // Iterate the cards array
   for (i in cards) {
+    // Create each card
     let cardElement = document.createElement('div');
-    cardElement.classList.add('card', 'col-3');
+    // Assign its classes
+    cardElement.classList.add('card', 'col-3', 'not-flipped');
+    // Assign its ID
     cardElement.setAttribute('id', cards[i].id);
+    // Assign its color
     cardElement.innerText = `${cards[i].color}`;
-    cardElement.onclick = () => flipCard(cardElement, cards);
+    // Add onClick function for flipping
+    cardElement.onclick = () => flipCard(cardElement);
+    // Display the element within the cardContainer
     cardContainer.appendChild(cardElement);
+  }
+}
+
+function flipCard(e) {
+  // Log for testing
+  console.log(e);
+
+  // If the card has already been matched
+  if (e.classList.contains('matched')) {
+    exit;
+  }
+  // If the clicked card is face-down (not-flipped)
+  if (e.classList.contains('not-flipped')) {
+    // Flip it face-up
+    e.classList.add('bold', 'flipped');
+    e.classList.remove('not-flipped');
+    // Assign its div ID as the value we check
+    if (cardFirst == null) {
+      cardFirst = e.id;
+    } else if (cardSecond == null) {
+      cardSecond = e.id;
+    }
+  }
+  // If we have selected 2 cards
+  if (cardFirst != null && cardSecond != null) {
+    // Call checkMatch
+    console.log(`Cards Chosen: ${cardFirst}, ${cardSecond}`);
+    checkMatch(cardFirst, cardSecond);
   }
 }
